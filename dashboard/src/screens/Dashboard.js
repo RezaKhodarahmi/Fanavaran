c553ce5102e7a2b4f9f2803db06264f29462c0c6
Payam <payam.mahjoub@gmail.com>
2023-01-14 16:59:27 +0330

    CHARTS Layout

--- dashboard/src/screens/Dashboard.js
+++ dashboard/src/screens/Dashboard.js
@@ -25,115 +25,34 @@ const Dashboard = () => {
         </ol>
       </nav>
 
-      <div className="container-fluid">Dashboard</div>
+      <div className="row mb-3">
+        <div className="col-12 col-sm-6 col-md-4">
+          <div className="container-fluid">CHART</div>
+        </div>
 
-      <div className="container-fluid mt-3">
-        <h1>Form</h1>
+        <div className="col-12 col-sm-6 col-md-4">
+          <div className="container-fluid">CHART</div>
+        </div>
 
-        <form>
-          <div className="mb-3">
-            <label for="exampleInputEmail1" className="form-label">
-              Email address
-            </label>
-            <input
-              type="email"
-              className="form-control"
-              id="exampleInputEmail1"
-              aria-describedby="emailHelp"
-            />
-            <div id="emailHelp" className="form-text">
-              We'll never share your email with anyone else.
-            </div>
-          </div>
-
-          <div className="mb-3">
-            <label for="exampleInputPassword1" className="form-label">
-              Password
-            </label>
-            <input
-              type="password"
-              className="form-control"
-              id="exampleInputPassword1"
-            />
-          </div>
-
-          <div className="mb-3 form-check">
-            <input
-              type="checkbox"
-              className="form-check-input"
-              id="exampleCheck1"
-            />
-            <label className="form-check-label" for="exampleCheck1">
-              Check me out
-            </label>
-          </div>
-
-          <div className="mb-3">
-            <label for="formFile" className="form-label">
-              Default file input example
-            </label>
-            <input className="form-control" type="file" id="formFile" />
-          </div>
-
-          <div className="mb-3">
-            <label for="exampleColorInput" className="form-label">
-              Color picker
-            </label>
-            <input
-              type="color"
-              className="form-control form-control-color"
-              id="exampleColorInput"
-              value="#563d7c"
-              title="Choose your color"
-            />
-          </div>
-
-          <div className="mb-3">
-            <select className="form-select" aria-label="Default select example">
-              <option selected="">Open this select menu</option>
-              <option value="1">One</option>
-              <option value="2">Two</option>
-              <option value="3">Three</option>
-            </select>
-          </div>
-          <button type="submit" className="btn btn-primary">
-            Submit
-          </button>
-        </form>
+        <div className="col-12 col-sm-6 col-md-4">
+          <div className="container-fluid">CHART</div>
+        </div>
       </div>
 
-      <div className="container-fluid mt-3">
-        <h1>Table</h1>
+      <div className="row mb-3">
+        <div className="col-12 col-sm-6 col-md-6">
+          <div className="container-fluid">CHART</div>
+        </div>
 
-        <table className="table table-striped table-dark">
-          <thead>
-            <tr>
-              <th scope="col">#</th>
-              <th scope="col">First</th>
-              <th scope="col">Last</th>
-              <th scope="col">Handle</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <th scope="row">1</th>
-              <td>Mark</td>
-              <td>Otto</td>
-              <td>@mdo</td>
-            </tr>
-            <tr>
-              <th scope="row">2</th>
-              <td>Jacob</td>
-              <td>Thornton</td>
-              <td>@fat</td>
-            </tr>
-            <tr>
-              <th scope="row">3</th>
-              <td colspan="2">Larry the Bird</td>
-              <td>@twitter</td>
-            </tr>
-          </tbody>
-        </table>
+        <div className="col-12 col-sm-6 col-md-6">
+          <div className="container-fluid">CHART</div>
+        </div>
+      </div>
+
+      <div className="row mb-3">
+        <div className="col-md-12">
+          <div className="container-fluid">CHART</div>
+        </div>
       </div>
     </>
   );
