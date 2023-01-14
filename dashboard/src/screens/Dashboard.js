ae9e26ef0a188909bf15059a9bae91204f5d29bc
Payam <payam.mahjoub@gmail.com>
2023-01-14 18:25:52 +0330

    Charts Part One with Components

--- dashboard/src/screens/Dashboard.js
+++ dashboard/src/screens/Dashboard.js
@@ -1,12 +1,38 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import env from "react-dotenv";
 import { Link } from "react-router-dom";
 import { useTranslation } from "react-i18next";
 import Mail from "../components/Mail";
 import { useDispatch } from "react-redux";
 import { sendRegisterNotif } from "../actions/mailActions";
+import BarChart from "../components/charts/BarChart";
+import LineChart from "../components/charts/LineChart";
+import PieChart from "../components/charts/PieChart";
+// Data
+import { UserData } from "../Data";
 
 const Dashboard = () => {
+  // Charts
+  const [userData, setUserData] = useState({
+    labels: UserData.map((data) => data.year),
+    datasets: [
+      {
+        label: "Users Gained",
+        data: UserData.map((data) => data.userGain),
+        backgroundColor: [
+          "rgba(75,192,192,1)",
+          "#ecf0f1",
+          "#50AF95",
+          "#f3ba2f",
+          "#2a71d0",
+        ],
+        borderColor: "black",
+        borderWidth: 2,
+      },
+    ],
+  });
+
+  //
   const { t } = useTranslation();
   const form = useRef();
   const dispatch = useDispatch();
@@ -29,15 +55,15 @@ const Dashboard = () => {
 
       <div className="row mb-3">
         <div className="col-12 col-sm-6 col-md-4">
-          <div className="container-fluid">CHART</div>
+          <div className="container-fluid"><div style={{ height: 400 }}><BarChart chartData={userData}/></div></div>
         </div>
 
         <div className="col-12 col-sm-6 col-md-4">
-          <div className="container-fluid">CHART</div>
+          <div className="container-fluid"><div style={{ height: 400 }}><PieChart chartData={userData}/></div></div>
         </div>
 
         <div className="col-12 col-sm-6 col-md-4">
-          <div className="container-fluid">CHART</div>
+          <div className="container-fluid"></div>
         </div>
       </div>
 
@@ -53,7 +79,7 @@ const Dashboard = () => {
 
       <div className="row mb-3">
         <div className="col-md-12">
-          <div className="container-fluid">CHART</div>
+          <div className="container-fluid"><div style={{ height: 400 }}><LineChart chartData={userData}/></div></div>
         </div>
       </div>
     </>
